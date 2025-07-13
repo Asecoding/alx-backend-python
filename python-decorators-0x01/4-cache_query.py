6713466fad25d80f905ff3427ed58097c70c5e67
Assefa.endale <abassiendale96@gmail.com>
2025-07-13 17:10:33 +0300

    updating cache query  file

--- python-decorators-0x01/4-cache_query.py
+++ python-decorators-0x01/4-cache_query.py
@@ -25,17 +25,3 @@ def retry_on_failure(retries=3, delay=2):
 
     return decorator
 
-
-@with_db_connection
-@cache_query
-def fetch_users_with_cache(conn, query):
-    cursor = conn.cursor()
-    cursor.execute(query)
-    return cursor.fetchall()
-
-
-#### First call will cache the result
-users = fetch_users_with_cache(query="SELECT * FROM users")
-
-#### Second call will use the cached result
-users_again = fetch_users_with_cache(query="SELECT * FROM users")
