17ffb18a3ff83eca3b10107e792ca3f938d0a1dd
Assefa Endale <76453266+Asecoding@users.noreply.github.com>
2025-07-15 13:13:50 +0300

    Update test_utils2.py

--- 0x03-Unittests_and_integration_tests/test_utils2.py
+++ 0x03-Unittests_and_integration_tests/test_utils2.py
@@ -10,14 +10,13 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    def test_get_json(self, test_url, test_payload):
-        with patch("utils.requests.get") as mock_get:
-            mock_response = Mock()
-            mock_response.json.return_value = test_payload
-            mock_get.return_value = mock_response
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
 
-            result = get_json(test_url)
+        result = get_json(test_url)
 
-            mock_get.assert_called_once_with(test_url)
-            self.assertEqual(result, test_payload)
-
+        mock_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_payload)
