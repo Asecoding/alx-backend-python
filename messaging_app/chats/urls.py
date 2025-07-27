f795c4f1dfc90da7780ecf9ebad7094fc5d33bd3
Assefa Endale <Abassiendale96@gmail.com>
2025-07-27 19:34:03 +0000

    updating urls file

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -9,3 +9,15 @@ router.register(r'messages', MessageViewSet, basename='message')
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
+
