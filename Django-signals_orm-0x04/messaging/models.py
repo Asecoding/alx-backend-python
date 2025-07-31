1340de209e11f7af1449062adec215e4b0cf7961
Assefa Endale <Abassiendale96@gmail.com>
2025-07-31 17:19:58 +0000

    updating models file

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -13,3 +13,13 @@ class Message(models.Model):
     def __str__(self):
         return f"{self.sender} → {self.receiver}: {self.content[:30]}"
 
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"🔔 Notification for {self.user.username} - Message ID {self.message.id}"
+
